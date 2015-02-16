facb753a21cc9867c83ac7d2fe32644e1854fe0d
Tristan Rice <rice@fn.lc>
2015-02-15 23:38:36 -0800

    Removed file

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ rm nnforge_wrap.cxx
 
 go build -x
 
-swig -go -intgosize 64 -module nnforge -I/usr/include/boost/tr1/tr1 -I/usr/include -I/usr/local/cuda/include -I/usr/local/cuDNNf -c++ nnforge.swigcxx
+#swig -go -intgosize 64 -module nnforge -I/usr/include/boost/tr1/tr1 -I/usr/include -I/usr/local/cuda/include -I/usr/local/cuDNNf -c++ nnforge.swigcxx
+rm nnforge_gc.c
+rm nnforge_wrap.cxx
