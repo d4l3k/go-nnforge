6306ebebad9ab49cb9d8f31362f85a5b9b774a2c
Tristan Rice <rice@fn.lc>
2015-02-15 23:31:11 -0800

    Swig functions

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@ export NNFORGE=nnForge/nnforge
 cp import.go $NNFORGE
 cp nnforge.swigcxx $NNFORGE
 
+rm nnforge.go
+rm nnforge_gc.c
+rm nnforge_wrap.cxx
+
 go build -x
+
+swig -go -intgosize 64 -module nnforge -I/usr/include/boost/tr1/tr1 -I/usr/include -I/usr/local/cuda/include -I/usr/local/cuDNNf -c++ nnforge.swigcxx
