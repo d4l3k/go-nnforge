8c56a2af254707086e3f98eec25ffb10a06a2752
Tristan Rice <rice@fn.lc>
2015-02-15 23:09:53 -0800

    Package builds now

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 cd nnForge/nnforge
 cp ../../import.go .
-cp ../../nn_types.h .
 cp ../../nnforge.swigcxx .
 
 rm nnforge.go
 rm nnforge_wrap.cxx
 rm nnforge_gc.c
 
-go build -x
+go build -x -work
--- import.go
+++ import.go
@@ -1,6 +1,7 @@
 package nnforge
 
-//#cgo CXXFLAGS: -I/usr/include/boost/tr1/tr1 -I/usr/include -I/usr/include -ffast-math -march=native  -mfpmath=sse -msse2  -O3   -c -lboost_thread-mt -lboost_regex-mt -lboost_chrono-mt -lboost_filesystem-mt -lboost_program_options-mt -lboost_random -lboost_system -lboost_date_time -lboost_program_options
+//#cgo CPPFLAGS: -I/usr/include/boost/tr1/tr1 -I/usr/include -ffast-math -march=native  -mfpmath=sse -msse2 -lopencv_highgui -lopencv_imgproc -lopencv_core -lcudnn -lcurand -lcusparse -lcublas -lcudart -lnetcdf -lnetcd  -O3    -lboost_thread-mt -lboost_regex-mt -lboost_chrono-mt -lboost_filesystem-mt -lboost_program_options-mt -lboost_random -lboost_system-mt -lboost_system -lboost_date_time -lboost_program_options -I/usr/local/cuda/include -I/usr/local/cuDNNf
+//#cgo LDFLAGS: -lopencv_highgui -lopencv_imgproc -lopencv_core -lboost_thread -lboost_regex -lboost_chrono -lboost_filesystem -lboost_program_options -lboost_random -lboost_system -lboost_date_time -lboost_program_options
 import "C"
 
 //#cgo CXXFLAGS: -std=c++11 -lboost_thread-mt -lboost_regex-mt -lboost_chrono-mt -lboost_filesystem-mt -lboost_program_options-mt -lboost_random -lboost_system -lboost_date_time -lboost_regex -ffast-math -march=native -mfpmath=sse -msse3 -I/usr/include/boost/tr1/tr1 -I/usr/include -I/usr/include/random -D NNFORGE_CPP11COMPILER
